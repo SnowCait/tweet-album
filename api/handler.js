5643e21b0180b079e096317a3d140640ebb9eeca
SnowCait <SnowCait@users.noreply.github.com>
2023-01-28 14:02:39 +0900

    Move authorizer

--- api/handler.js
+++ api/handler.js
@@ -21,49 +21,6 @@ const {
   albums_table: albumsTable,
 } = process.env;
 
-export const authorizer = async event => {
-  console.log('[event]', event);
-
-  const [ userId, authorizationAccessToken ] = event.headers.authorization.split(':');
-
-  const { Item: user } = await db.send(new GetCommand({
-    TableName: usersTable,
-    Key: {
-      twitterUserId: userId,
-    },
-  }));
-  console.log('[user]', user);
-
-  if (!user) {
-    return { isAuthorized: false };
-  }
-
-  const isAuthorized = (authorizationAccessToken === user.authorizationAccessToken);
-  let accessToken = authorizationAccessToken;
-
-  // Expired by system
-  if (user.twitterAccessToken !== user.authorizationAccessToken) {
-    await db.send(new UpdateCommand({
-      TableName: usersTable,
-      Key: {
-        twitterUserId: userId,
-      },
-      UpdateExpression: 'SET authorizationAccessToken = twitterAccessToken',
-    }));
-    accessToken = user.twitterAccessToken;
-  }
-
-  const response = {
-    isAuthorized,
-    context: {
-      userId,
-      accessToken,
-    },
-  };
-  console.log('[response]', response);
-  return response;
-};
-
 export const hello = async event => {
   return {
     statusCode: 200,
